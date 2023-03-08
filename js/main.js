88701da7fa30c029e899775168fa281214416651
Anastasiya Yacheykina <yacheykina_nastya@mail.ru>
2023-03-08 17:45:17 +0300

    Оптимизирует код (удаляет лишние переменные, упрощает функции)

--- js/main.js
+++ js/main.js
@@ -29,94 +29,29 @@ const COMMENT_NAME = [
   'Возьмак',
 ];
 
-const NUMBER_OF_ID_MIN = 1;
+const POST_COUNTER = 25;
 
-const NUMBER_OF_ID_MAX = 25;
+let photoId = 1;
+let commentsId = 1;
 
-const NUMBER_OF_URL_MIN = 1;
-
-const NUMBER_OF_URL_MAX = 25;
-
-const NUMBER_OF_LIKES_MIN = 15;
-
-const NUMBER_OF_LIKES_MAX = 200;
-
-const NUMBER_OF_COMMENT_URL_MIN = 1;
-
-const NUMBER_OF_COMMENT_URL_MAX = 6;
-
-const NUMBER_OF_COMMENTS_MIN = 1;
-
-const NUMBER_OF_COMMENTS_MAX = 10;
-
-
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-const createRandomNumberFromRangeGenerator = (min, max) => {
-  const previousValues = [];
-
-  return () => {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
-
-const createIdGenerator = () => {
-  let lastGeneratedId = 0;
-
-  return () => {
-    lastGeneratedId += 1;
-    return lastGeneratedId;
-  };
-};
-
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const generatePhotoId = createRandomNumberFromRangeGenerator(NUMBER_OF_ID_MIN, NUMBER_OF_ID_MAX); // Создает уникальный id в диапазоне от 1 до 25
+const generateComments = () => ({
+  id: commentsId++,
+  avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg',
+  message: Array.from(new Set(Array.from({length: getRandomInteger(1, 2)}, () => getRandomArrayElement(COMMENT_MESSAGE)))).join(' '),
+  name: getRandomArrayElement(COMMENT_NAME),
+});
 
-const generatePhotoUrl = createRandomNumberFromRangeGenerator(NUMBER_OF_URL_MIN, NUMBER_OF_URL_MAX); // Создает уникальный адрес url в диапазоне от 1 до 25
+const createPublication = () => ({
+  id: photoId,
+  url: 'photos/' + photoId++ + '.jpg',
+  description: getRandomArrayElement(PHOTO_DESCRIPTION),
+  likes: getRandomInteger(15, 200),
+  comments: Array.from({length: getRandomInteger(1, 10)}, generateComments),
+});
 
-const generateNumberOfLikes = createRandomNumberFromRangeGenerator(NUMBER_OF_LIKES_MIN, NUMBER_OF_LIKES_MAX); // Генерирует случайное количество лайков под фотографией в диапазоне от 15 до 200
+const increasePublications = () => Array.from({length: POST_COUNTER}, createPublication);
 
-const generateCommentId = createIdGenerator(); // Создает уникальный id комментария
-
-const generateCommentUrl = createRandomNumberFromRangeGenerator(NUMBER_OF_COMMENT_URL_MIN, NUMBER_OF_COMMENT_URL_MAX); // Создает адрес url аватара в диапазоне от 1 до 6
-
-const generateComments = () => {
-  return {
-    id: generateCommentId(),
-    avatar: 'img/avatar-' + generateCommentUrl() + '.svg',
-    message: getRandomArrayElement(COMMENT_MESSAGE),
-    name: getRandomArrayElement(COMMENT_NAME),
-  };
-};
-
-const numberOfComments = createRandomNumberFromRangeGenerator(NUMBER_OF_COMMENTS_MIN, NUMBER_OF_COMMENTS_MAX); // Генерирует случайно число комментариев от 1 до 10
-
-const createArray = Array.from({length: numberOfComments}, generateComments);
-
-const createPublication = () => {
-  return {
-    id: generatePhotoId(),
-    url: 'photos/' + generatePhotoUrl() + '.jpg',
-    description: getRandomArrayElement(PHOTO_DESCRIPTION),
-    likes: generateNumberOfLikes(),
-    comments: createArray,
-  };
-};
-
-const increasePublications = Array.from({length: 25}, createPublication);
+increasePublications();
